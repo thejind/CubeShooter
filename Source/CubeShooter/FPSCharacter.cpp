b2e67c8eec8824a6720734ef560585c11b1cd328
thejind <dsjindofficial@gmail.com>
2025-12-05 13:31:52 +0530

    Create Simple FPS Character

--- Source/CubeShooter/FPSCharacter.cpp
+++ Source/CubeShooter/FPSCharacter.cpp
@@ -1,34 +1,111 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "FPSCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/LocalPlayer.h"
+#include "Engine/World.h"
 
-// Sets default values
 AFPSCharacter::AFPSCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
+    GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
+    FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+    FPSCameraComponent->SetupAttachment(GetCapsuleComponent());
+    FPSCameraComponent->SetRelativeLocation(FVector(0, 0, 64.f));
+    FPSCameraComponent->bUsePawnControlRotation = true;
+
+    FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+    FPSMesh->SetupAttachment(FPSCameraComponent);
+    FPSMesh->SetOnlyOwnerSee(true);
+    FPSMesh->bCastDynamicShadow = false;
+    FPSMesh->CastShadow = false;
+    GetMesh()->SetOwnerNoSee(true);
+
+    MuzzleOffset = FVector(100.f, 0.f, 0.f);
 }
 
-// Called when the game starts or when spawned
 void AFPSCharacter::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
+    if (APlayerController* PC = Cast<APlayerController>(Controller))
+    {
+        if (ULocalPlayer* LocalPlayer = PC->GetLocalPlayer())
+        {
+            if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+            {
+                if (DefaultMappingContext)
+                {
+                    Subsystem->AddMappingContext(DefaultMappingContext, 0);
+                }
+            }
+        }
+    }
 }
 
-// Called every frame
-void AFPSCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
 void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    UEnhancedInputComponent* EnhancedInput = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+    if (MoveAction)
+    {
+        EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AFPSCharacter::Move);
+    }
+
+    if (LookAction)
+    {
+        EnhancedInput->BindAction(LookAction, ETriggerEvent::Triggered, this, &AFPSCharacter::Look);
+    }
+
+    if (JumpAction)
+    {
+        EnhancedInput->BindAction(JumpAction, ETriggerEvent::Started, this, &AFPSCharacter::JumpStarted);
+        EnhancedInput->BindAction(JumpAction, ETriggerEvent::Completed, this, &AFPSCharacter::JumpStopped);
+    }
+
+    if (FireAction)
+    {
+        EnhancedInput->BindAction(FireAction, ETriggerEvent::Started, this, &AFPSCharacter::Fire);
+    }
 }
 
+void AFPSCharacter::Move(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
+    if (Controller)
+    {
+        AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+        AddMovementInput(GetActorRightVector(), MovementVector.X);
+    }
+}
+
+void AFPSCharacter::Look(const FInputActionValue& Value)
+{
+    FVector2D LookAxisValue = Value.Get<FVector2D>();
+    if (Controller)
+    {
+        AddControllerYawInput(LookAxisValue.X);
+        AddControllerPitchInput(LookAxisValue.Y);
+    }
+}
+
+void AFPSCharacter::JumpStarted(const FInputActionValue& Value)
+{
+    Jump();
+}
+
+void AFPSCharacter::JumpStopped(const FInputActionValue& Value)
+{
+    StopJumping();
+}
+
+void AFPSCharacter::Fire(const FInputActionValue& Value)
+{
+    
+}
--- Source/CubeShooter/FPSCharacter.h
+++ Source/CubeShooter/FPSCharacter.h
@@ -1,10 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 #include "FPSCharacter.generated.h"
+
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+class ACubeProjectile;
 
 UCLASS()
 class CUBESHOOTER_API AFPSCharacter : public ACharacter
@@ -12,18 +18,43 @@ class CUBESHOOTER_API AFPSCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AFPSCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	// Enhanced Input Mapping Context
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// Input Actions
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> FireAction;
+
+	// Input callbacks
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void JumpStarted(const FInputActionValue& Value);
+	void JumpStopped(const FInputActionValue& Value);
+	void Fire(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* FPSCameraComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* FPSMesh;
+
+	UPROPERTY(EditAnywhere, Category = Gameplay)
+	FVector MuzzleOffset;
 };
