0aeafe719d2963d6138db03154481dd8cdef8882
thejind <dsjindofficial@gmail.com>
2025-12-05 16:16:12 +0530

    Game Instance to implement Join and Host session for players.

---
+++ Source/CubeShooter/ShooterGameInstance.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterGameInstance.h"
+#include "MultiplayerManager.h"
+
+void UShooterGameInstance::HostSessionWithCode(const FString& JoinCode)
+{
+	if (!MultiplayerManager)
+	{
+		MultiplayerManager = NewObject<UMultiplayerManager>(this);
+	}
+	MultiplayerManager->HostSession(JoinCode);
+}
+
+void UShooterGameInstance::JoinSessionWithCode(const FString& JoinCode)
+{
+	if (!MultiplayerManager)
+	{
+		MultiplayerManager = NewObject<UMultiplayerManager>(this);
+	}
+	MultiplayerManager->JoinSession(JoinCode);
+}
---
+++ Source/CubeShooter/ShooterGameInstance.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ShooterGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBESHOOTER_API UShooterGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category="Multiplayer")
+	void HostSessionWithCode(const FString& JoinCode);
+ 
+	UFUNCTION(BlueprintCallable, Category="Multiplayer")
+	void JoinSessionWithCode(const FString& JoinCode);
+ 
+private:
+	UPROPERTY()
+	TObjectPtr<UMultiplayerManager> MultiplayerManager;
+};
